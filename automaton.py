bacfa70f5640eaa50fabf47659cd9fb196ac740e
MejorJugadorDeKSanteSkibidiToilet <mancomancito@gmail.com>
2025-10-08 10:58:33 +0200

    Comentado

--- automaton.py
+++ automaton.py
@@ -13,6 +13,16 @@ from utils import is_deterministic
 
 class FiniteAutomaton:
     def __init__(self, initial_state, states, symbols, transitions, final_states):
+        """
+        Constructor for a finite automaton.
+
+        args:
+        initial_state: The automaton's starting state
+        states: a set of all states
+        symbols: alphabet of symbols
+        transitions: dictionary of transitions
+        final_states: a set of states that can be accepted
+        """
         self.initial_state = initial_state
         self.states = states
         self.symbols = symbols
@@ -20,11 +30,31 @@ class FiniteAutomaton:
         self.final_states = final_states
         
     def add_transition(self, start_state, symbol, end_state):
+        """
+        Adds a transition to the automaton
+
+
+        args:
+        start_state: The initial state of the transition
+        symbol: symbol that triggers the transition
+        final_states: The destination state of the transition
+        """
         if start_state not in self.transitions:
             self.transitions[start_state] = {}
         self.transitions[start_state][symbol] = end_state
 
     def accepts(self, cadena):
+        """
+        Checks if the automaton will accept a given string
+
+
+        args:
+        cadena: input string
+
+        returns:
+            True if automaton accepts the string
+            False if it doesn't
+        """
         state = self.initial_state
         for symbol in cadena:
             while True:
@@ -46,12 +76,26 @@ class FiniteAutomaton:
         return state in self.final_states
     
     def to_deterministic(self):
+        """   
+        Turns automaton into a deterministic DFA
+        """
         pass
 
     def to_minimized(self):
+        """
+        Turns automaton inta a minimized DFA
+        """
         pass
         
     def draw(self, path="./images/", filename="automata.png", view=False):
+        """
+        Draws the automaton
+
+        Args:
+        path: pathfile to save the resulting image
+        fiilename: name of the image
+        view: if true, opens the image after being generated
+        """
         dot = Digraph(comment="Automata", format="png")
         dot.attr(rankdir="LR")
 
