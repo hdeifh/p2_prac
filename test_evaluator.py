b83d8d02e49128d6b3c233f9f347ae62fec31593
hdeifh <110774134+hdeifh@users.noreply.github.com>
2025-10-31 13:01:52 +0100

    more testcases

--- test_evaluator.py
+++ test_evaluator.py
@@ -142,6 +142,95 @@ class TestEvaluatorNumber(TestEvaluatorBase):
         self._check_accept("0.0.0", should_accept=False)
         self._check_accept("0-0.0", should_accept=False)
 
+class TestEvaluatorBinaryEvenZeros(TestEvaluatorBase):
+    """Test for binary strings with an even number of zeros."""
+
+    def _create_automata(self):
+        description = """
+        Automaton:
+            Symbols: 01
+
+            even final
+            odd
+
+            ini even -0-> odd
+            ini even -1-> even
+            odd -0-> even
+            odd -1-> odd
+        """
+        return AutomataFormat.read(description)
+
+    def test_even_zeros(self):
+        """Test that the automaton accepts strings with an even number of 0s."""
+        self._check_accept("", should_accept=True)
+        self._check_accept("1", should_accept=True)
+        self._check_accept("0", should_accept=False)
+        self._check_accept("00", should_accept=True)
+        self._check_accept("010", should_accept=True)
+        self._check_accept("1010", should_accept=True)
+        self._check_accept("1111", should_accept=True)
+        self._check_accept("10001", should_accept=False)
+
+
+class TestEvaluatorEndsWithAB(TestEvaluatorBase):
+    """Test for strings that end with 'ab'."""
+
+    def _create_automata(self):
+        description = """
+        Automaton:
+            Symbols: ab
+
+            q0
+            q1
+            q2 final
+
+            ini q0 -a-> q1
+            q1 -b-> q2
+            q0 -b-> q0
+            q1 -a-> q1
+            q2 -a-> q1
+            q2 -b-> q0
+        """
+        return AutomataFormat.read(description)
+
+    def test_ends_with_ab(self):
+        """Test strings ending with 'ab'."""
+        self._check_accept("ab", should_accept=True)
+        self._check_accept("aab", should_accept=True)
+        self._check_accept("babab", should_accept=True)
+        self._check_accept("ba", should_accept=False)
+        self._check_accept("aabb", should_accept=False)
+        self._check_accept("", should_accept=False)
+
+class TestEvaluatorRepeatedWord(TestEvaluatorBase):
+    """Test for an automaton that accepts 'abc' repeated any number of times."""
+
+    def _create_automata(self):
+        description = """
+        Automaton:
+            Symbols: abc
+
+            q0
+            q1
+            q2
+            q3 final
+
+            ini q0 -a-> q1
+            q1 -b-> q2
+            q2 -c-> q3
+            q3 -a-> q1
+        """
+        return AutomataFormat.read(description)
+
+    def test_repeated_abc(self):
+        """Test repetition of 'abc' pattern."""
+        self._check_accept("abc", should_accept=True)
+        self._check_accept("abcabc", should_accept=True)
+        self._check_accept("abcabcabc", should_accept=True)
+        self._check_accept("ab", should_accept=False)
+        self._check_accept("abca", should_accept=False)
+        self._check_accept("abcc", should_accept=False)
+        self._check_accept("", should_accept=False)
 
 if __name__ == '__main__':
     unittest.main()
